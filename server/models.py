8d4b6fbdba5e4edbfdadcc45a8a6db0a1d589a9a
Linda Seiter <linseiter@gmail.com>
2023-06-19 09:02:09 -0400

    first commit

---
+++ server/models.py
@@ -0,0 +1,59 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+
+    # add relationship
+
+    # add serialization rules
+
+    def __repr__(self):
+        return f'<Hero {self.id}>'
+
+
+class Power(db.Model, SerializerMixin):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+
+    # add relationship
+
+    # add serialization rules
+
+    # add validation
+
+    def __repr__(self):
+        return f'<Power {self.id}>'
+
+
+class HeroPower(db.Model, SerializerMixin):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String, nullable=False)
+
+    # add relationships
+
+    # add serialization rules
+
+    # add validation
+
+    def __repr__(self):
+        return f'<HeroPower {self.id}>'
